847b10a0593dfd963f3628d51fa2b95a36853ef2
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-19 17:33:48 +0200

    progress on Model class

--- src/thermoelasticity_fem/model.py
+++ src/thermoelasticity_fem/model.py
@@ -58,6 +58,7 @@ class Model:
         for element in self.mesh.elements:
             mat_Muu_e = element.compute_mat_Muu_e()
             self.mat_M[element.dofs_nums_u, element.dofs_nums_u] += mat_Muu_e
+        self.mat_M = csc_array(self.mat_M)
 
     def assemble_K(self):
         self.mat_K = np.zeros((self.mesh.n_dofs, self.mesh.n_dofs))
@@ -68,6 +69,7 @@ class Model:
             self.mat_K[element.dofs_nums_u, element.dofs_nums_t] += mat_Kut_e
             mat_Ktt_e = element.compute_mat_Ktt_e()
             self.mat_K[element.dofs_nums_t, element.dofs_nums_t] += mat_Ktt_e
+        self.mat_K = csc_array(self.mat_K)
 
     def assemble_D(self):
         self.mat_D = np.zeros((self.mesh.n_dofs, self.mesh.n_dofs))
@@ -76,6 +78,48 @@ class Model:
             self.mat_D[element.dofs_nums_t, element.dofs_nums_u] += mat_Dtu_e
             mat_Dtt_e = element.compute_mat_Dtt_e()
             self.mat_D[element.dofs_nums_t, element.dofs_nums_t] += mat_Dtt_e
+        self.mat_D = csc_array(self.mat_D)
+
+    def assemble_F(self):
+        self.vec_F = np.zeros((self.mesh.n_dofs, ))
+
+        # Nodal forces
+        if self.dict_nodal_forces is not None:
+            for tag, vec_f in self.dict_nodal_forces.items():
+                nodes = self.mesh.dict_nodes_groups[tag]
+                for node in nodes:
+                    self.vec_F[(node * 4):(node * 4 + 3)] += vec_f
+        # Surface forces
+        if self.dict_surface_forces is not None:
+            for tag, vec_f in self.dict_surface_forces.items():
+                table_tri = self.mesh.dict_tri_groups[tag]
+                for i in range(table_tri.shape[0]):
+                    nodes = table_tri[i, :]
+                    X1 = self.mesh.table_nodes[nodes[0], :]
+                    X2 = self.mesh.table_nodes[nodes[1], :]
+                    X3 = self.mesh.table_nodes[nodes[2], :]
+                    X12 = X2 - X1
+                    X13 = X3 - X1
+                    area = 0.5 * np.abs(np.dot(X12, X13))
+                    for node in nodes:
+                        self.vec_F[(node * 4):(node * 4 + 3)] += area * vec_f / 3
+        # Volume forces
+        if self.dict_volume_forces is not None:
+            for tag, vec_f in self.dict_volume_forces.items():
+                table_tet = self.mesh.dict_tet_groups[tag]
+                for i in range(table_tet.shape[0]):
+                    nodes = table_tet[i, :]
+                    X1 = self.mesh.table_nodes[nodes[0], :]
+                    X2 = self.mesh.table_nodes[nodes[1], :]
+                    X3 = self.mesh.table_nodes[nodes[2], :]
+                    X4 = self.mesh.table_nodes[nodes[3], :]
+                    X12 = X2 - X1
+                    X13 = X3 - X1
+                    X14 = X4 - X1
+                    volume = np.abs(np.dot(X14, np.cross(X13, X12))) / 6
+                    for node in nodes:
+                        self.vec_F[(node * 4):(node * 4 + 3)] += volume * vec_f / 4
+        # Heat flux
 
     def clear_full_matvec(self):
         self.mat_M = None
