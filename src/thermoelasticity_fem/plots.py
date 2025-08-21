effe18fef9c39c7cceef6976045823a9b35123a2
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-21 13:01:29 +0200

    added animation function

--- src/thermoelasticity_fem/plots.py
+++ src/thermoelasticity_fem/plots.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pyvista as pv
 
 
@@ -13,3 +15,53 @@ def plot_U_T(mesh, temperature, displacement):
     p.add_mesh(warped_mesh, show_edges=True)
     p.show_axes()
     p.show()
+
+
+def animate_U_T(mesh, temperature, displacement, vec_t, save_path, fps=10, quality=5):
+    mio_mesh = mesh.make_meshio_mesh()
+
+    val_min = np.amin(temperature.flatten())
+    val_max = np.amax(temperature.flatten())
+
+    clim = [val_min, val_max]
+    cmap = plt.cm.get_cmap("jet")
+
+    p = pv.Plotter()
+    p.open_movie(save_path, framerate=fps, quality=quality)
+    pv_mesh = pv.from_meshio(mio_mesh)
+    pv_mesh.point_data['T'] = temperature
+    pv_mesh.set_active_scalars('T')
+    pv_mesh['U'] = displacement.reshape((mesh.n_nodes, 3))
+    warped_mesh = pv_mesh.warp_by_vector('U', factor=1.)
+    actor = p.add_mesh(
+        warped_mesh,
+        cmap=cmap,
+        clim=clim,
+        # scalar_bar_args={'background_color': [1.0, 1.0, 1.0]},
+        show_edges=True,
+        # interpolate_before_map=False
+    )
+    p.show_axes()
+    p.add_title(f't = {vec_t[0]:.1f} s')
+    p.write_frame()
+
+    for i in range(1, vec_t.size):
+        p.remove_actor(actor)
+        pv_mesh = pv.from_meshio(mio_mesh)
+        pv_mesh.point_data['T'] = temperature
+        pv_mesh.set_active_scalars('T')
+        pv_mesh['U'] = displacement.reshape((mesh.n_nodes, 3))
+        warped_mesh = pv_mesh.warp_by_vector('U', factor=1.)
+        actor = p.add_mesh(
+            warped_mesh,
+            cmap=cmap,
+            clim=clim,
+            # scalar_bar_args={'background_color': [1.0, 1.0, 1.0]},
+            show_edges=True,
+            # interpolate_before_map=False
+        )
+        p.show_axes()
+        p.add_title(f't = {vec_t[i]:.1f} s')
+        p.write_frame()
+
+    p.close()
