ab2e6028d084115564a425a213484670f786e689
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-21 13:06:52 +0200

    fixed animation function

--- src/thermoelasticity_fem/plots.py
+++ src/thermoelasticity_fem/plots.py
@@ -29,9 +29,9 @@ def animate_U_T(mesh, temperature, displacement, vec_t, save_path, fps=10, quali
     p = pv.Plotter()
     p.open_movie(save_path, framerate=fps, quality=quality)
     pv_mesh = pv.from_meshio(mio_mesh)
-    pv_mesh.point_data['T'] = temperature
+    pv_mesh.point_data['T'] = temperature[:, 0]
     pv_mesh.set_active_scalars('T')
-    pv_mesh['U'] = displacement.reshape((mesh.n_nodes, 3))
+    pv_mesh['U'] = displacement[:, 0].reshape((mesh.n_nodes, 3))
     warped_mesh = pv_mesh.warp_by_vector('U', factor=1.)
     actor = p.add_mesh(
         warped_mesh,
@@ -48,9 +48,9 @@ def animate_U_T(mesh, temperature, displacement, vec_t, save_path, fps=10, quali
     for i in range(1, vec_t.size):
         p.remove_actor(actor)
         pv_mesh = pv.from_meshio(mio_mesh)
-        pv_mesh.point_data['T'] = temperature
+        pv_mesh.point_data['T'] = temperature[:, i]
         pv_mesh.set_active_scalars('T')
-        pv_mesh['U'] = displacement.reshape((mesh.n_nodes, 3))
+        pv_mesh['U'] = displacement[:, i].reshape((mesh.n_nodes, 3))
         warped_mesh = pv_mesh.warp_by_vector('U', factor=1.)
         actor = p.add_mesh(
             warped_mesh,
