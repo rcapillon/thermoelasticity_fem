41d920f74831bc93b1f1d29b54a01030b7a9ae69
Rémi Capillon <rcapillon@yahoo.fr>
2025-09-07 17:48:07 +0200

    wip on reduced order model

--- src/thermoelasticity_fem/solvers.py
+++ src/thermoelasticity_fem/solvers.py
@@ -89,6 +89,9 @@ class LinearTransient:
         self.X = None
         self.Xdot = None
         self.Xdotdot = None
+        self.q = None
+        self.qdot = None
+        self.qdotdot = None
         self.U = None
         self.Udot = None
         self.Udotdot = None
@@ -213,3 +216,108 @@ class LinearTransient:
         self.T = self.X[3::4, :] + np.tile(reference_temperature[:, np.newaxis], (1, self.n_t))
         self.Tdot = self.Xdot[3::4, :]
         self.Tdotdot = self.Xdotdot[3::4, :]
+
+    def solve_ROM(self, n_modes_u, n_modes_theta):
+        self.model.create_free_dofs_lists()
+        self.model.assemble_M()
+        self.model.assemble_K()
+        self.model.assemble_D()
+        self.model.compute_ROB_u(n_modes_u)
+        self.model.compute_ROB_theta(n_modes_theta)
+        self.model.compute_ROM_matrices()
+
+        prev_q = np.zeros((self.model.n_q_u + self.model.n_q_t, ))
+        prev_qdot = np.zeros((self.model.n_q_u + self.model.n_q_t, ))
+        prev_qdotdot = np.zeros((self.model.n_q_u + self.model.n_q_t, ))
+
+        prev_q[:self.model.n_q_u] = self.model.mat_phi_u.transpose() @ self.initial_U
+        prev_q[self.model.n_q_u:] = self.model.mat_phi_t.transpose() @ self.initial_theta
+
+        prev_qdot[:self.model.n_q_u] = self.model.mat_phi_u.transpose() @ self.initial_Udot
+        prev_qdot[self.model.n_q_u:] = self.model.mat_phi_t.transpose() @ self.initial_thetadot
+
+        self.q = np.zeros((self.model.n_q_u + self.model.n_q_t, self.n_t))
+        self.qdot = np.zeros((self.model.n_q_u + self.model.n_q_t, self.n_t))
+        self.qdotdot = np.zeros((self.model.n_q_u + self.model.n_q_t, self.n_t))
+
+        self.q[:, 0] = prev_q
+        self.qdot[:, 0] = prev_qdot
+        # initial acceleration is assumed to be zero
+
+        mat_Kdyn_rom = (self.model.mat_Mrom
+                        + self.gamma * self.dt * self.model.mat_Drom
+                        + self.beta * (self.dt ** 2) * self.model.mat_Krom)
+
+        for i in tqdm(range(1, self.n_t)):
+            self.model.assemble_F(idx_t=i)
+            self.model.compute_ROM_forces()
+
+            rhs = (self.model.vec_From
+                   - self.model.mat_Drom @ (prev_qdot + (1 - self.gamma) * self.dt * prev_qdotdot)
+                   - self.model.mat_Krom @ (prev_q + self.dt * prev_qdot
+                                             + 0.5 * (self.dt ** 2) * (1 - 2 * self.beta) * prev_qdotdot))
+
+            new_qdotdot = spsolve(mat_Kdyn_rom, rhs)
+            new_qdot = (prev_qdot
+                        + (1 - self.gamma) * self.dt * prev_qdotdot
+                        + self.gamma * self.dt * new_qdotdot)
+            new_q = (prev_q
+                     + self.dt * prev_qdot
+                     + 0.5 * (self.dt ** 2) * ((1 - 2 * self.beta) * prev_qdotdot + 2 * self.beta * new_qdotdot))
+
+            self.q[:, i] = new_q
+            self.qdot[:, i] = new_qdot
+            self.qdotdot[:, i] = new_qdotdot
+
+            prev_q = new_q
+            prev_qdot = new_qdot
+            prev_qdotdot = new_qdotdot
+
+        self.U = np.zeros((self.model.mesh.n_nodes * 3, self.n_t))
+        self.Udot = np.zeros((self.model.mesh.n_nodes * 3, self.n_t))
+        self.Udotdot = np.zeros((self.model.mesh.n_nodes * 3, self.n_t))
+        self.T = np.zeros((self.model.mesh.n_nodes, self.n_t))
+        self.Tdot = np.zeros((self.model.mesh.n_nodes, self.n_t))
+        self.Tdotdot = np.zeros((self.model.mesh.n_nodes, self.n_t))
+
+        if self.model.dict_dirichlet_U is not None:
+            for tag, list_u_dir in self.model.dict_dirichlet_U.items():
+                dirichlet_nodes_U = self.model.mesh.dict_tri_groups[tag].flatten()
+                dirichlet_nodes_U = list(set(dirichlet_nodes_U))
+                for node in dirichlet_nodes_U:
+                    for str_dof, val_u in list_u_dir:
+                        if str_dof == 'x':
+                            modif = 0
+                        elif str_dof == 'y':
+                            modif = 1
+                        elif str_dof == 'z':
+                            modif = 2
+                        else:
+                            raise ValueError('Unrecognized DOF.')
+                        self.U[node * 3 + modif, :] = val_u
+        if self.model.dict_dirichlet_theta is not None:
+            for tag, theta in self.model.dict_dirichlet_theta.items():
+                dirichlet_nodes_T = self.model.mesh.dict_tri_groups[tag].flatten()
+                dirichlet_nodes_T = list(set(dirichlet_nodes_T))
+                for node in dirichlet_nodes_T:
+                    self.T[node, :] = theta
+
+        self.U[self.model.free_dofs_U, :] = self.model.mat_phi_u @ self.q[:self.model.n_q_u, :]
+        self.Udot[self.model.free_dofs_U, :] = self.model.mat_phi_u @ self.qdot[:self.model.n_q_u, :]
+        self.Udotdot[self.model.free_dofs_U, :] = self.model.mat_phi_u @ self.qdotdot[:self.model.n_q_u, :]
+
+        reference_temperature = np.zeros((self.model.mesh.n_nodes,))
+        for i in range(self.model.mesh.n_nodes):
+            n_count = 0
+            for tag, material in self.model.mesh.dict_materials.items():
+                table_tet = self.model.mesh.dict_tet_groups[tag]
+                list_nodes = list(set(list(table_tet.flatten())))
+                if i in list_nodes:
+                    n_count += 1
+                    reference_temperature[i] += material.T0
+            reference_temperature[i] /= n_count
+
+        self.T += np.tile(reference_temperature[:, np.newaxis], (1, self.n_t))
+        self.T[self.model.free_dofs_theta, :] = self.model.mat_phi_t @ self.q[self.model.n_q_u:, :]
+        self.Tdot[self.model.free_dofs_theta, :] = self.model.mat_phi_t @ self.qdot[self.model.n_q_u:, :]
+        self.Tdotdot[self.model.free_dofs_theta, :] = self.model.mat_phi_t @ self.qdotdot[self.model.n_q_u:, :]
