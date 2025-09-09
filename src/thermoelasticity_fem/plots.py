cf48a78eed835287d073677a7f66e1e894ee225c
Rémi Capillon <rcapillon@yahoo.fr>
2025-09-09 16:53:22 +0200

    wip uncertainty modeling

--- src/thermoelasticity_fem/plots.py
+++ src/thermoelasticity_fem/plots.py
@@ -74,12 +74,12 @@ def animate_U_T(mesh, temperature, displacement, vec_t, save_path,
     p.close()
 
 
-def plot_dofU_vs_t(dof_num, vec_t, displacement, save_path=None):
+def plot_dofU_vs_t(Udof_num, vec_t, displacement, save_path=None):
     _, ax = plt.subplots()
-    ax.plot(vec_t, displacement[dof_num, :], '-b')
+    ax.plot(vec_t, displacement[Udof_num, :].flatten(), '-b')
     ax.set_xlabel('t [s]')
     ax.set_ylabel('displacement [m]')
-    ax.set_title(f'Displacement, DOF {dof_num}')
+    ax.set_title(f'Displacement, U_DOF {Udof_num}')
     ax.grid()
 
     if save_path is not None:
@@ -90,11 +90,75 @@ def plot_dofU_vs_t(dof_num, vec_t, displacement, save_path=None):
 
 def plot_nodeT_vs_t(node_num, vec_t, temperature, save_path=None):
     _, ax = plt.subplots()
-    ax.plot(vec_t, temperature[node_num, :], '-b')
+    ax.plot(vec_t, temperature[node_num, :].flatten(), '-b')
     ax.set_xlabel('t [s]')
     ax.set_ylabel('Temperature [degrees]')
     ax.set_title(f'Temperature, node {node_num}')
     ax.grid()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
+def plot_dofU_vs_t_random(vec_t, displacement_random, displacement_deterministic,
+                          confidence_level=None, save_path=None):
+    Udof_mean = np.mean(displacement_random[:, :], axis=-1)
+
+    _, ax = plt.subplots()
+    ax.plot(vec_t, Udof_mean.flatten(), '-b', label='statistical mean')
+    ax.plot(vec_t, displacement_deterministic, '-r', label='deterministic response')
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel('displacement [m]')
+    ax.set_title('Displacement, observed dof')
+    ax.legend()
+    ax.grid()
+
+    if confidence_level is not None:
+        n_rejected_samples = int(np.floor((1 - confidence_level) * displacement_random.shape[2] / 2))
+        ordered_samples = np.zeros((displacement_random.shape[1], displacement_random.shape[2]))
+        lower_confidence_bound = np.zeros((displacement_random.shape[1], ))
+        upper_confidence_bound = np.zeros((displacement_random.shape[1], ))
+        for i in range(displacement_random.shape[1]):
+            ordered_samples[i, :] = np.sort(displacement_random[i, :])
+            lower_confidence_bound[i] = ordered_samples[i, n_rejected_samples]
+            upper_confidence_bound[i] = ordered_samples[i, -(n_rejected_samples + 1)]
+        ax.plot(vec_t, lower_confidence_bound, '--g', label=f'{confidence_level * 100}% confidence interval')
+        ax.plot(vec_t, upper_confidence_bound, '--g')
+        ax.fill_between(vec_t, lower_confidence_bound, upper_confidence_bound, color='cyan')
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
+def plot_nodeT_vs_t_random(vec_t, temperature_random, temperature_deterministic,
+                          confidence_level=None, save_path=None):
+    Tnode_mean = np.mean(temperature_random, axis=-1)
+
+    _, ax = plt.subplots()
+    ax.plot(vec_t, Tnode_mean.flatten(), '-b', label='statistical mean')
+    ax.plot(vec_t, temperature_deterministic, '-r', label='deterministic response')
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel('temperature [degrees]')
+    ax.set_title('Temperature, observed node')
+    ax.legend()
+    ax.grid()
+
+    if confidence_level is not None:
+        n_rejected_samples = int(np.floor((1 - confidence_level) * temperature_random.shape[2] / 2))
+        ordered_samples = np.zeros((temperature_random.shape[1], temperature_random.shape[2]))
+        lower_confidence_bound = np.zeros((temperature_random.shape[1], ))
+        upper_confidence_bound = np.zeros((temperature_random.shape[1], ))
+        for i in range(temperature_random.shape[1]):
+            ordered_samples[i, :] = np.sort(temperature_random[i, :])
+            lower_confidence_bound[i] = ordered_samples[i, n_rejected_samples]
+            upper_confidence_bound[i] = ordered_samples[i, -(n_rejected_samples + 1)]
+        ax.plot(vec_t, lower_confidence_bound, '--g', label=f'{confidence_level * 100}% confidence interval')
+        ax.plot(vec_t, upper_confidence_bound, '--g')
+        ax.fill_between(vec_t, lower_confidence_bound, upper_confidence_bound, color='cyan')
 
     if save_path is not None:
         plt.savefig(save_path)
